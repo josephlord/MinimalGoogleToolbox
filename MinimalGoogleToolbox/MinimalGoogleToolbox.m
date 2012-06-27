a92618c26782a16673f8b886cf1c80791ca973c9
Joseph Lord <joseph@human-friendly.com>
2012-06-27 09:47:26 +0100

    Initial Commit

---
+++ MinimalGoogleToolbox/MinimalGoogleToolbox.m
@@ -0,0 +1,13 @@
+//
+//  MinimalGoogleToolbox.m
+//  MinimalGoogleToolbox
+//
+//  Created by Joseph Lord on 27/06/2012.
+//  Copyright (c) 2012 Human Friendly Ltd. All rights reserved.
+//
+
+#import "MinimalGoogleToolbox.h"
+
+@implementation MinimalGoogleToolbox
+
+@end

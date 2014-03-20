3d8b1c3e121e40658bbe07fd5d04c88e482bd228
Joseph Lord <joseph@human-friendly.com>
2014-03-20 18:01:29 +0000

    Adds a dummy class so that the library does not appear empty and warning is not raised in Xcode. Shouldn't be necessary.

---
+++ MinimalGoogleToolbox/DummyClass.h
@@ -0,0 +1,13 @@
+//
+//  DummyClass.h
+//  MinimalGoogleToolbox
+//
+//  Created by Joseph on 20/03/2014.
+//  Copyright (c) 2014 Human Friendly Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyClass : NSObject
+// The sole purpose of the class is to remove a warning as otherwise there are only categories.
+@end
---
+++ MinimalGoogleToolbox/DummyClass.m
@@ -0,0 +1,13 @@
+//
+//  DummyClass.m
+//  MinimalGoogleToolbox
+//
+//  Created by Joseph on 20/03/2014.
+//  Copyright (c) 2014 Human Friendly Ltd. All rights reserved.
+//
+
+#import "DummyClass.h"
+
+@implementation DummyClass
+
+@end
